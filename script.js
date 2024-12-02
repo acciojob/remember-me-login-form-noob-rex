a33338a3b95437ed1968ca7dc42be9ae74f23c26
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-12-02 19:33:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+let nameInput = document.getElementById('username');
+let passwordInput = document.getElementById("password");
+let checkInput = document.getElementById("checkbox");
+let sbutton = document.getElementById("submit");
+var name;
+var password;
+var check;
+
+nameInput.addEventListener('input', () => {
+    name = nameInput.value;
+    // console.log(name)
+});
+
+passwordInput.addEventListener('input', () => {
+    password = passwordInput.value;
+});
+
+checkInput.addEventListener("input", () => {
+      check = checkInput.checked;
+      // console.log('check');
+});
+
+sbutton.addEventListener('click', () => {
+    if(check){
+      localStorage.setItem('username',name);
+      localStorage.setItem('password',password);
+    }
+    alert(`Logged in as ${name}`);
+});
